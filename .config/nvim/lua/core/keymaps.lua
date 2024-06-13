9bfe0d7047e4320705993e1a730b536b9376dd5f
paul-talma <109061871+paul-talma@users.noreply.github.com>
2024-06-13 16:50:55 -0400

    terminal keymap

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -9,7 +9,7 @@ map("i", "jk", "<ESC>", { desc = "Exit insert mode with 'jk'" })
 
 -- splits
 map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-map("n", "<leader>sh", "<C-w>s", { desc = "Split window horiszontally" })
+map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 -- Move to start/end of line
@@ -21,7 +21,9 @@ map("n", "j", "gj", opts)
 map("n", "k", "gk", opts)
 
 -- scroll lock
-map("n", "<leader>sc", function() vim.opt.scrolloff = 999 - vim.o.scrolloff end, { desc = "Toggle scroll lock" })
+map("n", "<leader>sc", function()
+	vim.opt.scrolloff = 999 - vim.o.scrolloff
+end, { desc = "Toggle scroll lock" })
 
 -- Map enter to ciw in normal mode
 map("n", "<CR>", "ciw", opts)
@@ -33,10 +35,15 @@ map("n", "<leader>;", "<cmd>Alpha<CR>", { desc = "Dashboard" })
 -- search current buffer
 map("n", "<C-s>", ":Telescope current_buffer_fuzzy_find<CR>", opts)
 
+-- terminal escape
+map("t", "<Esc>", "<C-\\><C-n>")
+map("n", "<leader>t", "<C-w>v<cmd>terminal<CR>i", { desc = "Open terminal to the right" })
+
+-- TODO: make better mapping
 -- compile and run C++ code
-map(
-	"n",
-	"<leader>c",
-	":w<CR>:!g++ -std=c++20 % -o %<.out && %<.out<CR>",
-	{ desc = "Compile c++ file and run if successful", noremap = true, silent = true }
-)
+-- map(
+-- 	"n",
+-- 	"<leader>c",
+-- 	":w<CR>:!g++ -std=c++20 % -o %<.out && %<.out<CR>",
+-- 	{ desc = "Compile c++ file and run if successful", noremap = true, silent = true }
+-- )
