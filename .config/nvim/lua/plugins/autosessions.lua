2166ff312d9f5b5cded65f42c6bcabe597e8f794
paul-talma <109061871+paul-talma@users.noreply.github.com>
2024-06-13 16:51:18 -0400

    misc.

--- .config/nvim/lua/plugins/autosessions.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"rmagatti/auto-session",
-	config = function()
-		require("auto-session").setup({
-			auto_restore_enabled = false,
-			})
-		end,
-}
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
 return {
 	{
 		"sainnhe/gruvbox-material",
+		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme gruvbox-material")
+			vim.cmd.colorscheme("gruvbox-material")
 		end,
+	},
+	{
+		"askfiy/visual_studio_code",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("visual_studio_code")
+		end,
+	},
 }
-}
-
